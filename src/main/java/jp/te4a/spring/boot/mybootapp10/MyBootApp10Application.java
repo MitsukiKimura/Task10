c269feb3266d18d92acd7ca8fb72f24ed513f1c3
木村 光希 <180012@jc-21.jp>
2021-06-14 13:31:17 +0900

    owari

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp10/MyBootApp10Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.mybootapp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApp10Application.class, args);
+    }
+
+}
